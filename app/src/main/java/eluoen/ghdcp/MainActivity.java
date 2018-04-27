369c0eba084aaaebb078fe4cf1a0f8e9698a141c
eluoen7 <eluoen@163.com>
2018-04-27 17:47:37 +0800

    第一次提交测试

--- app/src/main/java/eluoen/ghdcp/MainActivity.java
+++ app/src/main/java/eluoen/ghdcp/MainActivity.java
@@ -2,6 +2,7 @@ package eluoen.ghdcp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"第一次github测试",Toast.LENGTH_LONG).show();
     }
 }
